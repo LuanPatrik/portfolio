f2a8dfa23ff183476c6425cab444e387230b553c
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-04-27 17:24:30 -0300

    Continuacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <header>
+        <ul>
+          <li>Home</li>
+          <li>Skills</li>
+          <li>Projetos</li>
+        </ul>
+      </header>
+      <main>
+        <div className="information">
+          <h2>Luan Patrik</h2>
+          <h3>Desenvolvedor de Software e Admirador da área de Business Intelligence</h3>
+          
+        </div>
+        <div className="image">
+
+        </div>
+      </main>
+      <footer>
+        <span>Create by Luan Patrik</span>
+      </footer>
+    </div>
   )
 }
 
