ccef5110fba1664d30868e0d0c47f63adc7d4192
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-05-06 01:04:02 -0300

    Atualizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import Logo from './components/img/logo.png';
 import GitHub from './components/img/github.png';
 import Linkedin from './components/img/linkedin.png';
 import Instagram from './components/img/instagram.png';
-import JavaScript from './components/img/javascript.png';
 import React from './components/img/react.png';
+import JavaScript from './components/img/javascript.png';
+import Csharp from './components/img/csharp.png';
+import MySql from './components/img/mysql.png';
+import PowerBi from './components/img/power-bi.png';
+import Trello from './components/img/trello.png';
 
 function App() {
 
@@ -23,8 +27,9 @@ function App() {
         </ul>
       </header>
 
-      <section id='home'>
+      <div id='home'>
         <div className="information">
+          <h1>Sobre Mim</h1>
           <p>
             Desenvolvedor de software e admirador de BI.<br />
             Atualmente atuo como estagiário em Desenvolvimento Web, <br />sou membro da empresa júnior OneBit do IFMG - 
@@ -47,35 +52,53 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
+      </div>
         
-      <section id='skills'>
-        <h3>Minhas Skills</h3>
+      <div id='skills'>
+        <h1>Minhas Skills</h1>
         <br />
         <div className="languages">
           <div className="reactjs" id='card'>
             <img src={React} alt="React" />
           </div>
-          <div className="c#" id='card'>
-
+          <div className="csharp" id='card'>
+            <img src={Csharp} alt="Csharp" />
           </div>
           <div className="js" id='card'>
             <img src={JavaScript} alt="Javascript" />
           </div>
           <div className="mysql" id='card'>
-
+            <img src={MySql} alt="MySql" />
+          </div>
+          <div className="trello" id='card'>
+            <img src={Trello} alt="Trello" />
+          </div>
+          <div className="powerbi" id='card'>
+            <img src={PowerBi} alt="PowerBi" />
           </div>
         </div>
 
-      </section>
+      </div>
 
-      <section id='projects'>
+      <div id='projects'>
+        <h1>Projetos</h1>
+        <br />
 
-      </section>
+        <div className="list-projects">
+          <div class="list">
+            <img src={"img_avatar.png"} alt="Avatar" style="width:100%" />
+            <div class="description">
+              <h4><b>John Doe</b></h4>
+              <p>Architect & Engineer</p>
+            </div>
+          </div>
 
-      {/* <footer>
+        </div>
+      </div>
+
+      <footer>
         <span>Create by Luan Patrik</span>
-      </footer> */}
+      </footer>
     </div>
   )
 }
